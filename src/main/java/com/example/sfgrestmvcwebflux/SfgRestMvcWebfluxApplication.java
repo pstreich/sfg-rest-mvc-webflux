cc9fd2e616ef14660fa72802968917862eeedee0
pstreicher <pstreichstrk@gmail.com>
2022-11-18 09:08:08 +0100

    initial commit

---
+++ src/main/java/com/example/sfgrestmvcwebflux/SfgRestMvcWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgrestmvcwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRestMvcWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRestMvcWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfgrestmvcwebflux/SfgRestMvcWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgrestmvcwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRestMvcWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
